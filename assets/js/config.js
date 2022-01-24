abf69c843c6b32c9652c9bfdf817d6f4d5959076
Daniel Sundman <daniel@yds.se>
2022-01-24 15:59:43 +0100

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,14 @@
+var config = {
+  apiKey: "AIzaSyCQl1hXNyoVg8fP0dxKQpgY9qPkpnZUDVM",
+  authDomain: "mai-auth-test.firebaseapp.com",
+  projectId: "mai-auth-test",
+  storageBucket: "mai-auth-test.appspot.com",
+  messagingSenderId: "740600784457",
+  appId: "1:740600784457:web:55cc90e9fb2c193a820198",
+  measurementId: "G-231RL1MGBJ"
+};
+firebase.initializeApp(config);
+
+alert('FixItNowOrDeleteIt');
+
+var CLIENT_ID = null;
